a3a363d6bce2eb6db1488cb03f067ecaa6e2076f
Hassan Kakeh <hassan_kakeh@proton.me>
2022-10-22 22:36:45 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from "./app/footer";
 import Header from "./app/header";
 import ProjectList from "./app/project-list";
 import React from "react";
+import FadeInSection from "./app/fade-in-section";
 
 function App() {
   React.useEffect(() => {}, []);
@@ -24,50 +25,52 @@ function App() {
     <div className="App">
       <Header></Header>
       <div style={{ maxWidth: "1320px", margin: "0 auto" }}>
-        <section id="about" className="mt-4">
-          <h2 id="projects" className="text-center title mb-5">
-            About
-          </h2>
-          <div className="row w-100">
-            <div className="col-4">
-              <div
-                style={{ width: "50%", margin: "auto", cursor: "pointer" }}
-                className=" position-relative resume"
-                onClick={downloadCV}
-              >
-                <img
-                  style={{
-                    boxShadow:
-                      "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
-                  }}
-                  className="w-100 "
-                  src={process.env.PUBLIC_URL + "./my-cv.jpg"}
-                  alt=""
-                />
-                <span
-                  className="white d-none"
-                  style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%,-50%)",
-                    fontWeight: 600,
-                  }}
+        <FadeInSection>
+          <section id="about" className="mt-4">
+            <h2 id="projects" className="text-center title mb-5">
+              About
+            </h2>
+            <div className="row w-100">
+              <div className="col-4">
+                <div
+                  style={{ width: "50%", margin: "auto", cursor: "pointer" }}
+                  className=" position-relative resume"
+                  onClick={downloadCV}
                 >
-                  Download CV
-                </span>
+                  <img
+                    style={{
+                      boxShadow:
+                        "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+                    }}
+                    className="w-100 "
+                    src={process.env.PUBLIC_URL + "./my-cv.jpg"}
+                    alt=""
+                  />
+                  <span
+                    className="white d-none"
+                    style={{
+                      position: "absolute",
+                      top: "50%",
+                      left: "50%",
+                      transform: "translate(-50%,-50%)",
+                      fontWeight: 600,
+                    }}
+                  >
+                    Download CV
+                  </span>
+                </div>
+              </div>
+              <div className="col d-flex align-items-center">
+                <p className="larg-text">
+                  Front End Developer with one years experience building and
+                  maintaining responsive websites in the recruiting industry.
+                  using HTML, CSS, JavaScript,Angular and React; <br /> plus
+                  modern libraries and frameworks.
+                </p>
               </div>
             </div>
-            <div className="col">
-              <p className="larg-text">
-                Front End Developer with one years experience building and
-                maintaining responsive websites in the recruiting industry.
-                using HTML, CSS, JavaScript,Angular and React; <br /> plus
-                modern libraries and frameworks.
-              </p>
-            </div>
-          </div>
-        </section>
+          </section>
+        </FadeInSection>
         <ProjectList></ProjectList>
       </div>
       <Footer></Footer>
